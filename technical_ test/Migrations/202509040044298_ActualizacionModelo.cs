320c266faef6a976891f9ecec13a863be510ef31
emanuelb-o <emanuelbuilesochoa@gmail.com>
2025-09-03 19:10:56 -0600

    Agregar ProductController y migración ActualizacionModelo

---
+++ technical_ test/Migrations/202509040044298_ActualizacionModelo.cs
@@ -0,0 +1,18 @@
+﻿namespace technical__test.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ActualizacionModelo : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "GuidCode", c => c.Guid(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "GuidCode");
+        }
+    }
+}
---
+++ technical_ test/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace technical__test
+{
+    public class ProductController
+    {
+        public List<Product> GetAll(string filtro = "")
+        {
+            using (var db = new MiDbContext())
+            {
+                var query = db.Product.AsQueryable();
+                if (!string.IsNullOrWhiteSpace(filtro))
+                {
+                    query = query.Where(p => p.Name.Contains(filtro));
+                }
+               return query.ToList();
+            }
+        }
+
+        public Product GetById(int id)
+        {
+            using (var db = new MiDbContext())
+            {
+                return db.Product.Find(id);
+            }
+        }
+
+        public void Save(Product prod)
+        {
+            using (var db = new MiDbContext())
+            using (var transaction = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    if (prod.ProductID == 0)
+                    {
+                        prod.GuidCode = Guid.NewGuid();
+                        prod.CreationDate = DateTime.Now;
+                        db.Product.Add(prod);
+                    }
+                    else
+                    {
+                        prod.ModificationDate = DateTime.Now;
+                        db.Entry(prod).State = System.Data.Entity.EntityState.Modified;
+                    }
+
+                    db.SaveChanges();
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var db = new MiDbContext())
+            {
+                var prod = db.Product.Find(id);
+                if (prod != null)
+                {
+                    db.Product.Remove(prod);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
